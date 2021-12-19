63ef144fda17562296fb662b7e032ede9e2a4828
msmehwish <96256744+msmehwish@users.noreply.github.com>
2021-12-19 11:53:12 -0600

    adding test file

--- sample_script.py
+++ sample_script.py
@@ -3,11 +3,12 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 # init driver
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(executable_path="C:\\Users\\mehwi\\Desktop\\cd Automation\\python-selenium-automation\\chromedriver.exe")
 driver.maximize_window()
 
 # open the url
 driver.get('https://www.google.com/')
+sleep(3)
 
 search = driver.find_element(By.NAME, 'q')
 search.clear()
@@ -18,6 +19,7 @@ sleep(4)
 
 # click search
 driver.find_element(By.NAME, 'btnK').click()
+sleep(3)
 
 # verify
 assert 'dress' in driver.current_url.lower(), f"Expected query not in {driver.current_url.lower()}"
